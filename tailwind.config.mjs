9c30b73489594789d8cdf037f8c9d1aed5a73320
Sarah Albaek <saal0002@stud.kea.dk>
2024-09-13 13:41:42 +0200

    farver

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,16 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: { 
+            colors: {
+            'sort': '#1A1A1A',
+            'hvid': '#F4F4F4',
+            'grøn': '#CDF545',
+            'orange': '#FED12E',
+            'pink': '#FEB3D2',
+            'lilla': '#7E86FF',
+            'blå': '#36B7FF',
+          },},
 	},
 	plugins: [
         require('@tailwindcss/typography'),
